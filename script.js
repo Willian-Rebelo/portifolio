d6da5aaced6dbd96f4794253dfc35c71cd8976ed
Willian Rebelo de França <107209823+Willian-Rebelo@users.noreply.github.com>
2023-02-06 22:38:12 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const button = document.querySelector('button');
-const card = document.querySelector('.card-background');
-const closeButton = document.querySelector('.card-close');
+// const button = document.querySelector('button');
+// const card = document.querySelector('.card-background');
+// const closeButton = document.querySelector('.card-close');
 
-button.addEventListener('click', () => {
-    card.style.display = 'block'
-})
+// button.addEventListener('click', () => {
+//     card.style.display = 'block'
+// })
 
-closeButton.addEventListener('click', () => {
-    card.style.display = 'none'
-})
+// closeButton.addEventListener('click', () => {
+//     card.style.display = 'none'
+// })
 
